d25e4bc62b088045e11f1042a7590ed586d24f0d
Hao-Kang <haok1402@gmail.com>
2023-11-26 15:52:33 -0500

    Update RobotOptions and NodeBody component

--- website/src/components/Workflow/RobotNode.tsx
+++ website/src/components/Workflow/RobotNode.tsx
@@ -15,7 +15,7 @@ export interface RobotNodeDataProps {
 }
 
 export const RobotOptions = {
-  Text: ["gpt-3.5-turbo"],
+  Text: ["gpt-3.5-turbo", "gpt-4", "gpt-3.5-turbo-16k", "gpt-4-32k"],
 };
 
 interface NodeBodyProps extends BoxProps {
@@ -23,10 +23,23 @@ interface NodeBodyProps extends BoxProps {
   setRobotType: React.Dispatch<React.SetStateAction<string>>;
   robotSource: string;
   setRobotSource: React.Dispatch<React.SetStateAction<string>>;
+  system: string;
+  setSystem: React.Dispatch<React.SetStateAction<string>>;
+  prompt: string;
+  setPrompt: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const NodeBody: React.FC<NodeBodyProps> = React.memo(
-  ({ robotType, setRobotType, robotSource, setRobotSource }) => {
+  ({
+    robotType,
+    setRobotType,
+    robotSource,
+    setRobotSource,
+    system,
+    setSystem,
+    prompt,
+    setPrompt,
+  }) => {
     return (
       <Box
         sx={{
@@ -63,11 +76,44 @@ const NodeBody: React.FC<NodeBodyProps> = React.memo(
           onChange={(_, newValue) => {
             setRobotSource(newValue || "");
           }}
+          sx={{ marginBottom: 3 }}
           renderInput={(params) => <TextField {...params} label="Source" />}
           PaperComponent={({ children }) => (
             <Paper className="nodrag">{children}</Paper>
           )}
         />
+        {(() => {
+          switch (`${robotType}-${robotSource}`) {
+            case "Text-gpt-3.5-turbo":
+            case "Text-gpt-4":
+            case "Text-gpt-3.5-turbo-16k":
+            case "Text-gpt-4-32k":
+              return (
+                <>
+                  <TextField
+                    multiline
+                    maxRows={25}
+                    label="system"
+                    className="nodrag"
+                    value={system}
+                    onChange={(e) => setSystem(e.target.value)}
+                    sx={{ width: "100%", marginBottom: 3 }}
+                  />
+                  <TextField
+                    multiline
+                    maxRows={25}
+                    label="prompt"
+                    className="nodrag"
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                    sx={{ width: "100%" }}
+                  />
+                </>
+              );
+            default:
+              return <></>;
+          }
+        })()}
       </Box>
     );
   },
@@ -82,9 +128,21 @@ const RobotNode: React.FC<RobotNodeProps> = React.memo(({ data }) => {
   const [editableName, setEditableName] = useState(false);
   const [robotType, setRobotType] = useState("Text");
   const [robotSource, setRobotSource] = useState("gpt-3.5-turbo");
+  const [system, setSystem] = useState(
+    [
+      "You're a helpful assistant tasked with generating personalized cover letters.",
+      "By analyzing the job description and applicant's resume, your mission is to craft compelling cover letters that showcase the candidate's qualifications, ensuring they stand out and align with the job requirements.",
+    ].join("\n\n"),
+  );
+  const [prompt, setPrompt] = useState(
+    [
+      ["Job Description:", "{{ Job Description }}"].join("\n"),
+      ["Candidate's Resume:", "{{ Candidate's Resume }}"].join("\n"),
+    ].join("\n\n"),
+  );
   return (
     <>
-      <Paper elevation={3} sx={{ minWidth: "300px" }}>
+      <Paper elevation={3} sx={{ minWidth: "450px" }}>
         <NodeHeader
           name={name}
           setName={setName}
@@ -96,6 +154,10 @@ const RobotNode: React.FC<RobotNodeProps> = React.memo(({ data }) => {
           setRobotType={setRobotType}
           robotSource={robotSource}
           setRobotSource={setRobotSource}
+          system={system}
+          setSystem={setSystem}
+          prompt={prompt}
+          setPrompt={setPrompt}
         />
       </Paper>
     </>
