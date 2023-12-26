9e671ecec93e0d1d178bfaf01523af2f0edee7ec
Hao-Kang <haok1402@gmail.com>
2023-12-26 17:20:09 -0500

    populate initial nodes & edges
    style attribution to fit with background

--- src/components/Workflow.tsx
+++ src/components/Workflow.tsx
@@ -1,17 +1,38 @@
+import styled from "@emotion/styled";
+import { Theme } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import Box from "@mui/system/Box";
 import React from "react";
 import ReactFlow from "reactflow";
 import { Background, BackgroundVariant } from "reactflow";
 
 import BuildPanel from "src/components/Workflow/BuildPanel";
+import { useWorkflow } from "src/contexts/Workflow";
+
+const StyledReactFlow = styled(ReactFlow)<{ theme: Theme }>`
+  .react-flow__attribution {
+    background-color: ${({ theme }) => theme.palette.background.default};
+  }
+`;
 
 const Workflow = React.memo(() => {
+  const theme = useTheme();
+  const { nodes, edges } = useWorkflow();
+  const { onNodesChange, onEdgesChange, onConnect } = useWorkflow();
   return (
     <Box style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow proOptions={{ hideAttribution: true }}>
+      <StyledReactFlow
+        theme={theme}
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <BuildPanel />
         <Background variant={BackgroundVariant.Dots} />
-      </ReactFlow>
+      </StyledReactFlow>
     </Box>
   );
 });
--- src/contexts/Workflow.tsx
+++ src/contexts/Workflow.tsx
@@ -1,20 +1,69 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useCallback, useContext, useState } from "react";
+import { Node, OnNodesChange, applyNodeChanges } from "reactflow";
+import { Edge, OnEdgesChange, applyEdgeChanges } from "reactflow";
+import { OnConnect, addEdge } from "reactflow";
 
 const WorkflowContext = createContext<{
+  nodes: Node[];
+  edges: Edge[];
+  onNodesChange: OnNodesChange;
+  onEdgesChange: OnEdgesChange;
+  onConnect: OnConnect;
+
   buildType: "fc:input" | "fc:robot" | "fc:output";
   setBuildType: (buildType: "fc:input" | "fc:robot" | "fc:output") => void;
 }>({
+  nodes: [],
+  edges: [],
+  onNodesChange: () => {},
+  onEdgesChange: () => {},
+  onConnect: () => {},
+
   buildType: "fc:input",
   setBuildType: () => {},
 });
 
+const initialNodes: Node[] = [
+  { id: "1", data: { label: "Node 1" }, position: { x: 5, y: 5 } },
+  { id: "2", data: { label: "Node 2" }, position: { x: 5, y: 100 } },
+];
+
+const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }];
+
 const WorkflowProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes],
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges],
+  );
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges],
+  );
+
   const [buildType, setBuildType] = useState<
     "fc:input" | "fc:robot" | "fc:output"
   >("fc:input");
 
   return (
-    <WorkflowContext.Provider value={{ buildType, setBuildType }}>
+    <WorkflowContext.Provider
+      value={{
+        nodes,
+        edges,
+        onNodesChange,
+        onEdgesChange,
+        onConnect,
+
+        buildType,
+        setBuildType,
+      }}
+    >
       {children}
     </WorkflowContext.Provider>
   );
