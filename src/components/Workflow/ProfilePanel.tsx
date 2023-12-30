08ebad277211ec7f4f625be96912d4403f1a965e
Hao-Kang <haok1402@gmail.com>
2023-12-30 17:23:02 -0500

    switch to default profile without button

--- src/components/Workflow/ProfilePanel.tsx
+++ src/components/Workflow/ProfilePanel.tsx
@@ -1,6 +1,4 @@
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import { useTheme } from "@mui/material/styles";
 import React from "react";
 import { Panel } from "reactflow";
 
@@ -8,18 +6,10 @@ import { useFirebase } from "src/contexts/Firebase";
 
 const ProfilePanel = React.memo(() => {
   const { user } = useFirebase();
-  const { spacing } = useTheme();
 
   return (
-    <Panel
-      position="top-right"
-      style={{
-        transform: `translate(-${spacing(1)}, -${spacing(1)})`,
-      }}
-    >
-      <IconButton>
-        <Avatar src={user?.photoURL || ""} />
-      </IconButton>
+    <Panel position="top-right">
+      <Avatar src={user?.photoURL || ""} />
     </Panel>
   );
 });
