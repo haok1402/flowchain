7f617e56d71757f7e1eab9d27073966c84ecb070
Hao-Kang <haok1402@gmail.com>
2024-01-02 18:38:44 -0500

    switch runtime to be more supportive

--- functions/handlers/entry.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from firebase_functions import https_fn
-from middlewares.authentication import require_authentication, CallableRequest
-
-
-@https_fn.on_call()
-@require_authentication
-def entry_document(req: CallableRequest) -> Any:
-    return req.auth.token["email"]
--- functions/main.py
+++
@@ -1,5 +0,0 @@
-from firebase_admin import initialize_app
-
-initialize_app()
-
-from handlers.entry import *
