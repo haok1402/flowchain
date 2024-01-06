40fda5da8b16445f24969ff54a6fbc7d01dccd18
Hao-Kang <haok1402@gmail.com>
2024-01-05 23:28:45 -0500

    organize better by extracting shared comonents between different nodes

---
+++ website/src/components/Workflow/Nodes/Shared.tsx
@@ -0,0 +1,114 @@
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/styles";
+import React from "react";
+import { useCallback, useEffect, useState } from "react";
+import { MdClose, MdHelp } from "react-icons/md";
+import { getConnectedEdges } from "reactflow";
+import { Node } from "reactflow";
+
+import { useWorkflow } from "@contexts/Workflow";
+import { NodeData } from "@contexts/Workflow";
+
+interface NodeLabelProps {
+  id: string;
+  data: NodeData;
+}
+
+export const NodeLabel: React.FC<NodeLabelProps> = React.memo(
+  ({ id, data }) => {
+    const { typography } = useTheme();
+
+    const [editing, setEditing] = useState(false);
+    const handleTypographyOnDoubleClick = useCallback(() => {
+      setEditing(true);
+    }, []);
+    const handleTextFieldOnBlur = useCallback(() => {
+      setEditing(false);
+    }, []);
+
+    const [label, setLabel] = useState(data.label);
+    const handleLabelOnChange = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLabel(e.target.value);
+      },
+      [],
+    );
+
+    const { setNodes } = useWorkflow();
+    useEffect(() => {
+      setNodes((value) =>
+        value.map((node) => {
+          if (node.id === id) {
+            node.data = {
+              ...node.data,
+              label,
+            };
+          }
+          return node;
+        }),
+      );
+    }, [id, label, setNodes]);
+
+    return editing ? (
+      <TextField
+        fullWidth
+        autoFocus
+        autoComplete="off"
+        variant="standard"
+        value={label}
+        onBlur={handleTextFieldOnBlur}
+        onChange={handleLabelOnChange}
+        inputProps={{
+          style: {
+            padding: 0,
+            fontSize: typography.body1.fontSize,
+          },
+        }}
+      />
+    ) : (
+      <Typography onDoubleClick={handleTypographyOnDoubleClick}>
+        {label}
+      </Typography>
+    );
+  },
+);
+
+interface NodeActionProps {
+  id: string;
+  data: NodeData;
+}
+
+export const NodeAction: React.FC<NodeActionProps> = React.memo(({ id }) => {
+  const { nodes, edges, setNodes, setEdges } = useWorkflow();
+  const handleMdCloseOnClick = useCallback(() => {
+    let nodeToDelete = null;
+    const newNodes = nodes.reduce((acc, node) => {
+      if (node.id === id) {
+        nodeToDelete = node;
+      } else {
+        acc.push(node);
+      }
+      return acc;
+    }, [] as Node<NodeData>[]);
+    if (nodeToDelete !== null) {
+      setNodes(newNodes);
+      const connectedEdges = getConnectedEdges([nodeToDelete], edges);
+      const newEdges = edges.filter((edge) => !connectedEdges.includes(edge));
+      setEdges(newEdges);
+    }
+  }, [id, nodes, edges, setNodes, setEdges]);
+
+  return (
+    <Box>
+      <IconButton>
+        <MdHelp />
+      </IconButton>
+      <IconButton>
+        <MdClose onClick={handleMdCloseOnClick} />
+      </IconButton>
+    </Box>
+  );
+});
--- website/src/components/Workflow/Nodes/Target.tsx
+++ website/src/components/Workflow/Nodes/Target.tsx
@@ -1,74 +1,19 @@
+import { NodeAction, NodeLabel } from "./Shared";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import IconButton from "@mui/material/IconButton";
 import Skeleton from "@mui/material/Skeleton";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { ImExit } from "react-icons/im";
-import { MdClose, MdHelp } from "react-icons/md";
 import { MdContentCopy } from "react-icons/md";
 import { Handle, Position } from "reactflow";
-import { Node, NodeProps } from "reactflow";
-import { getConnectedEdges } from "reactflow";
+import { NodeProps } from "reactflow";
 
-import { useWorkflow } from "@contexts/Workflow";
-import { NodeData } from "@contexts/Workflow";
 import { NodeData as TargetNodeData } from "@shared/workflow/nodes/target";
-
-interface CardTitleProps {
-  label: string;
-  handleLabelOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-const CardLabel: React.FC<CardTitleProps> = React.memo(
-  ({ label, handleLabelOnChange }) => {
-    const { typography } = useTheme();
-    const [editing, setEditing] = useState(false);
-
-    return editing ? (
-      <TextField
-        fullWidth
-        autoFocus
-        autoComplete="off"
-        variant="standard"
-        value={label}
-        onBlur={() => setEditing(false)}
-        onChange={handleLabelOnChange}
-        inputProps={{
-          style: {
-            padding: 0,
-            fontSize: typography.body1.fontSize,
-          },
-        }}
-      />
-    ) : (
-      <Typography onDoubleClick={() => setEditing(true)}>{label}</Typography>
-    );
-  },
-);
-
-interface CardActionProps {
-  handleDeleteOnClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
-}
-
-const CardAction: React.FC<CardActionProps> = React.memo(
-  ({ handleDeleteOnClick }) => {
-    return (
-      <Box>
-        <IconButton>
-          <MdHelp />
-        </IconButton>
-        <IconButton onClick={handleDeleteOnClick}>
-          <MdClose />
-        </IconButton>
-      </Box>
-    );
-  },
-);
 
 interface TargetNodeProps extends NodeProps {
   data: TargetNodeData;
@@ -77,62 +22,15 @@ interface TargetNodeProps extends NodeProps {
 const TargetNode: React.FC<TargetNodeProps> = React.memo(({ id, data }) => {
   const { spacing, palette } = useTheme();
 
-  const [label, setLabel] = useState(data.label);
-  const handleLabelOnChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setLabel(e.target.value);
-    },
-    [],
-  );
-
-  const { setNodes } = useWorkflow();
-  useEffect(() => {
-    setNodes((value) =>
-      value.map((node) => {
-        if (node.id === id) {
-          node.data = {
-            ...node.data,
-            label,
-          };
-        }
-        return node;
-      }),
-    );
-  }, [id, setNodes, label]);
-
-  const { nodes, edges, setEdges } = useWorkflow();
-  const handleDeleteOnClick = useCallback(() => {
-    let nodeToDelete = null;
-    const newNodes = nodes.reduce((acc, node) => {
-      if (node.id === id) {
-        nodeToDelete = node;
-      } else {
-        acc.push(node);
-      }
-      return acc;
-    }, [] as Node<NodeData>[]);
-    if (nodeToDelete !== null) {
-      setNodes(newNodes);
-      const connectedEdges = getConnectedEdges([nodeToDelete], edges);
-      const newEdges = edges.filter((edge) => !connectedEdges.includes(edge));
-      setEdges(newEdges);
-    }
-  }, [id, nodes, edges, setNodes, setEdges]);
-
   return (
     <>
       <Handle type="target" position={Position.Left} />
       <Card sx={{ width: spacing(45) }}>
         <CardHeader
           avatar={<ImExit />}
-          title={
-            <CardLabel
-              label={label}
-              handleLabelOnChange={handleLabelOnChange}
-            />
-          }
+          title={<NodeLabel id={id} data={data} />}
           subheader="Display result of the workflow."
-          action={<CardAction handleDeleteOnClick={handleDeleteOnClick} />}
+          action={<NodeAction id={id} data={data} />}
         />
         <CardContent>
           <Box
